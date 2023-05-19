36d06c9ee003a647c37a20ee83fff43b2866346e
Axe <ngsmo@iscte-iul.pt>
2023-05-19 12:37:29 +0100

    start deploying SF

--- packages/contracts/script/PostDeploy.s.sol
+++ packages/contracts/script/PostDeploy.s.sol
@@ -5,13 +5,31 @@ import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 
+// get Superfluid deployer
+import {
+  SuperfluidFrameworkDeployer
+} from "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol";
+
 contract PostDeploy is Script {
+
+  SuperfluidFrameworkDeployer internal sfDeployer;
+
+  SuperfluidFrameworkDeployer.Framework internal sf;
+
   function run(address worldAddress) external {
+    console.log("PostDeploy.run()");
     // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
     // Start broadcasting transactions from the deployer account
     vm.startBroadcast(deployerPrivateKey);
+
+    sfDeployer = new SuperfluidFrameworkDeployer();
+    sf = sfDeployer.getFramework();
+    console.log("Resolver", address(sf.resolver));
+    console.log("Host", address(sf.host));
+    //resolver = sf.resolver;
+
     vm.stopBroadcast();
   }
 }
